29dee8157ecde9da9aa69ae1937e2e2b18cac12a
Paolo Adame <padame@my.centennialcollege.ca>
2025-11-27 18:51:25 -0500

    Update api-auth.js

--- client/lib/api-auth.js
+++ client/lib/api-auth.js
@@ -1,27 +1,29 @@
-const BASE_URL = import.meta.env.VITE_API_URL; 
-// VITE_API_URL = 'https://job-application-project-renderdeploy.onrender.com'
+const API_URL = import.meta.env.VITE_API_URL;
 
-const API_AUTH = `${import.meta.env.VITE_API_URL}/api/users`; // no trailing slash
-
-export const signin = async (user) => {
-  const response = await fetch(`${API_AUTH}/signin`, { // no trailing slash
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    credentials: 'include',
-    body: JSON.stringify(user)
-  });
-  return await response.json();
+const signin = async (user) => {
+    try {
+        let response = await fetch(`${API_URL}/auth/signin`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(user),
+        });
+        return await response.json();
+    } catch (err) {
+        console.log("Signin API Error:", err);
+    }
 };
 
-export const signout = async () => {
-  try {
-    const response = await fetch(`${API_AUTH}/signout`, {
-      method: 'GET',
-      credentials: 'include'
-    });
-    return await response.json();
-  } catch (err) {
-    console.error('Signout API Error:', err);
-    throw err;
-  }
+const signout = async () => {
+    try {
+        let response = await fetch(`${API_URL}/auth/signout`, { method: "GET" });
+        return await response.json();
+    } catch (err) {
+        console.log("Signout API Error:", err);
+    }
 };
+
+export { signin, signout };
